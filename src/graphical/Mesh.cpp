2968294db5170feed8552300e1c8d4e3fb733ade
Arthur Mendes <arthurmco@gmail.com>
2016-04-24 23:35:57 -0300

    Added mesh class

---
+++ src/graphical/Mesh.cpp
@@ -0,0 +1,77 @@
+#include "Mesh.hpp"
+
+using namespace Tribalia::Graphics;
+
+Mesh::Mesh()
+    : SceneObject("Mesh", glm::vec3(0,0,0), 0, 0, 0)
+{
+    _translMatrix = glm::mat4(1.0);
+    _scaleMatrix = glm::mat4(1.0);
+    _rotMatrix = glm::mat4(1.0);
+
+    _modelMatrix = glm::mat4(1.0);
+}
+
+void Mesh::Translate(glm::vec3 pos)
+{
+    _isModelChanged = true;
+    _translMatrix *= glm::translate(pos);
+}
+
+void Mesh::Scale(glm::vec3 scalef)
+{
+    _isModelChanged = true;
+    _scaleMatrix = glm::scale(scalef);
+}
+
+/* warning: rotate angle should be in radians */
+void Mesh::Rotate(glm::vec3 axis, float angle)
+{
+    _isModelChanged = true;
+    _rotMatrix *= glm::rotate(angle, axis);
+}
+
+void Mesh::ApplyTransformations()
+{
+    if (_isModelChanged) {
+        _modelMatrix = _translMatrix * _scaleMatrix * _rotMatrix;
+        _isModelChanged = false;
+    }
+}
+
+glm::mat4 Mesh::GetModelMatrix() const
+{
+    return _modelMatrix;
+}
+
+void Mesh::SetPosition(glm::vec3 pos)
+{
+    SceneObject::SetPosition(pos);
+    this->Translate(_position);
+    this->ApplyTransformations();
+}
+
+void Mesh::AddPosition(glm::vec3 pos)
+{
+    SceneObject::AddPosition(pos);
+    this->Translate(_position);
+    this->ApplyTransformations();
+}
+
+void Mesh::SetRotation(float x, float y, float z)
+{
+    SceneObject::SetRotation(x, y, z);
+    this->Rotate(glm::vec3(1,0,0), _rotations[0]);
+    this->Rotate(glm::vec3(0,1,0), _rotations[1]);
+    this->Rotate(glm::vec3(0,0,1), _rotations[2]);
+    this->ApplyTransformations();
+}
+
+void Mesh::AddRotation(float x, float y, float z)
+{
+    SceneObject::AddRotation(x, y, z);
+    this->Rotate(glm::vec3(1,0,0), _rotations[0]);
+    this->Rotate(glm::vec3(0,1,0), _rotations[1]);
+    this->Rotate(glm::vec3(0,0,1), _rotations[2]);
+    this->ApplyTransformations();
+}
--- src/graphical/Mesh.hpp
+++ src/graphical/Mesh.hpp
@@ -4,3 +4,53 @@
     Copyright (C) 2016 Arthur M.
 
 ***/
+
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+
+#include "SceneObject.hpp"
+
+#ifndef MESH_HPP
+#define MESH_HPP
+
+namespace Tribalia {
+namespace Graphics {
+
+    class Mesh : public SceneObject
+    {
+    private:
+        glm::mat4 _rotMatrix;
+        glm::mat4 _scaleMatrix;
+        glm::mat4 _translMatrix;
+
+        bool _isModelChanged = false;
+        glm::mat4 _modelMatrix;
+
+        void Translate(glm::vec3);
+        void Scale(glm::vec3);
+
+        /* warning: rotate angle should be in radians */
+        void Rotate(glm::vec3 axis, float angle);
+
+
+    public:
+        Mesh();
+
+        void SetPosition(glm::vec3);
+        void AddPosition(glm::vec3);
+
+        void SetRotation(float x, float y, float z);
+        void AddRotation(float x, float y, float z);
+
+        void ApplyTransformations();
+        glm::mat4 GetModelMatrix() const;
+
+    };
+
+
+} /* Graphics */
+} /* Tribalia */
+
+
+
+#endif /* end of include guard: MESH_HPP */
