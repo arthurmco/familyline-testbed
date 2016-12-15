0780dd61a3641a4f9260afebaed09e292ecfd11f
Arthur Mendes <arthurmco@gmail.com>
2016-12-15 14:09:39 -0200

    Updated scripts to take account of the new folders

--- scripts/distbin.bat
+++ scripts/distbin.bat
@@ -1,8 +1,30 @@
 @echo off
-echo Copying shaders...
+echo Copying data files...
 cd ..
+
 mkdir Debug\shaders
 mkdir Release\shaders
+mkdir Debug\materials
+mkdir Release\materials
+mkdir Debug\models
+mkdir Release\models
+
 copy shaders Debug\shaders
 copy shaders Release\shaders
-echo Shaders copied
+copy materials Debug\materials
+copy materials Release\materials
+copy models Debug\models
+copy models Release\models
+
+copy test.taif Debug
+copy test.taif Release
+
+copy *.bmp Debug
+copy *.bmp Release
+copy *.png Debug
+copy *.png Release
+
+cp *.md Debug
+cp *.md Release
+
+echo Data files copied
