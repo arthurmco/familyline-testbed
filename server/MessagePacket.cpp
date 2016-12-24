c56c8b6d40e72343561cfc02a18d4f444aa7c752
Arthur Mendes <arthurmco@gmail.com>
2016-12-23 22:51:51 -0200

    Started work on the game server app

---
+++ server/MessagePacket.cpp
@@ -0,0 +1,16 @@
+#include "MessagePacket.hpp"
+
+using namespace Tribalia::Server;
+
+MessagePacket::MessagePacket()
+{
+	_raw_msg_header = new RawMessagePacket;
+}
+
+MessagePacket::MessagePacket(unsigned char* data)
+{
+	_raw_message = data;
+	_raw_msg_header = (RawMessagePacket*) data;
+}
+
+
---
+++ server/Server.cpp
@@ -0,0 +1,14 @@
+/***
+ 	Tribalia server entry point	
+
+	Copyright (C) 2016 Arthur M
+***/
+
+#include "../src/EnviroDefs.h"
+
+#include <cstdio>
+
+int main(int argc, char const* argv[]) 
+{
+	printf("Server test\n");
+}	
