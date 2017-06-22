07d6a9dd72955819e190f9cac8dd602837faa5d4
Arthur Mendes <arthurmco@gmail.com>
2017-06-22 02:17:23 -0300

    Add a image-display GUI control
    Currently, the image display control only supports PNG, but it works,
    and scales the image to the control size

---
+++ src/graphical/gui/ImageControl.cpp
@@ -0,0 +1,75 @@
+#include "ImageControl.hpp"
+
+using namespace Tribalia::Graphics::GUI;
+
+/* Load the image from a filename.
+   Because it uses Cairo file loading functions directly, it only 
+   supports loading from PNG files */
+ImageControl::ImageControl(int x, int y, int w, int h, const char* file)
+    : IPanel(x,y,w,h)
+{
+    _bgColor = glm::vec4(255, 255, 255, 0);
+    _image = cairo_image_surface_create_from_png(file);
+
+}
+
+ImageControl::ImageControl(double x, double y, double w, double h,
+			   const char* file)
+    : IPanel(x,y,w,h, true)
+{
+    _bgColor = glm::vec4(255, 255, 255, 0);
+    
+    _image = cairo_image_surface_create_from_png(file);
+
+    auto st = cairo_surface_status(_image);
+    if (st != CAIRO_STATUS_SUCCESS) {
+        const char* ststr;
+
+	switch (st) {
+	case CAIRO_STATUS_FILE_NOT_FOUND: ststr = "File not found"; break;
+	default: ststr = "Unknown error"; break;
+	}
+
+	Log::GetLog()->Fatal("gui-image-control", "Error trying to open %s: %s",
+			     file, ststr);
+    }
+
+    _image_ctxt = cairo_create(_image);
+    double x2, x1, y2, y1;
+    cairo_clip_extents(_image_ctxt, &x1, &y1, &x2, &y2);
+
+    _imgW = x2-x1;
+    _imgH = y2-y1;
+
+}
+
+/* Load the image from a texture file object */
+ImageControl::ImageControl(int x, int y, int w, int h, TextureFile* f)
+    : IPanel(x,y,w,h)
+{
+    
+}
+
+ImageControl::ImageControl(double x, double y, double w, double h, TextureFile* f)
+    : IPanel(x,y,w,h,true)
+{
+
+}
+
+void ImageControl::Redraw(cairo_t* ctxt)
+{
+    cairo_save(ctxt); 
+    double sw = _width/_imgW;;
+    double sh = _height/_imgH;
+
+    cairo_scale(ctxt, sw, sh);    
+    cairo_set_source_surface(ctxt, _image, 0.0, 0.0);
+    cairo_paint(ctxt);
+    
+    cairo_restore(ctxt);
+}
+
+
+
+
+
---
+++ src/graphical/gui/ImageControl.hpp
@@ -0,0 +1,49 @@
+/*  Class for creating a control that draws an image
+    
+    Copyright (C) 2016 Arthur M
+*/
+
+#include "IPanel.hpp"
+#include "../TextureOpener.hpp"
+#include <string>
+#include <cstdarg>
+#include <cstring>
+
+#ifndef IMAGECONTROL_HPP
+#define IMAGECONTROL_HPP
+
+namespace Tribalia {
+namespace Graphics {
+namespace GUI {
+
+class ImageControl : public IPanel {
+private:
+    double _imgW, _imgH;
+    
+    cairo_surface_t* _image;
+    cairo_t* _image_ctxt;
+    
+public:
+    /* Load the image from a filename.
+       Because it uses Cairo file loading functions directly, it only 
+       supports loading from PNG files */
+    ImageControl(int x, int y, int w, int h, const char* file);
+    ImageControl(double x, double y, double w, double h, const char* file);
+
+    /* Load the image from a texture file object */
+    ImageControl(int x, int y, int w, int h, TextureFile* f);
+    ImageControl(double x, double y, double w, double h, TextureFile* f);
+    
+    virtual void Redraw(cairo_t* ctxt) override;
+
+    virtual bool ProcessInput(Input::InputEvent& ev) {return false;}
+
+    virtual void OnFocus() override {}
+    virtual void OnLostFocus() override {}
+};
+
+}
+}
+}
+
+#endif
