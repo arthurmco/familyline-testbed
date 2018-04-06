441f11ef56a7c381ee1db9723cd3a074f390348c
Arthur Mendes <arthurmco@gmail.com>
2018-04-05 23:57:24 -0300

    Make ANSI escape sequences work in cmd.exe

--- src/Tribalia.cpp
+++ src/Tribalia.cpp
@@ -151,6 +151,29 @@ int main(int argc, char const *argv[])
 	if (!fLog) {
 		if (isatty(fileno(stderr))) {
 			fLog = stderr;
+			
+#ifdef _WIN32
+			// Set output mode to handle virtual terminal sequences, so the
+			// color escape sequences can work on Windows
+			HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+			if (hOut == INVALID_HANDLE_VALUE)
+			{
+				return GetLastError();
+			}
+
+			DWORD dwMode = 0;
+			if (!GetConsoleMode(hOut, &dwMode))
+			{
+				return GetLastError();
+			}
+
+			dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+			if (!SetConsoleMode(hOut, dwMode))
+			{
+				return GetLastError();
+			}
+#endif
+
 		}
 		else {
 			errno = 0; // no need to worry
