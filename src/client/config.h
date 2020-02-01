387a6a5421d78e6a14f5402d2a4443941b7aee0f
Arthur Mendes <arthurmco@gmail.com>
2020-02-01 11:28:58 -0300

    Put files into the new directories

---
+++ src/client/config.h
@@ -0,0 +1,6 @@
+#define SHADERS_DIR "./shaders/"
+#define ICONS_DIR "./icons/"
+#define MATERIALS_DIR "./materials/"
+#define MODELS_DIR "./models/"
+#define TEXTURES_DIR "./textures/"
+#define ASSET_FILE_DIR "./"
