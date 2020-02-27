7c1fcc8fae32a437474de7e2548157fc391ab997
Arthur Mendes <arthurmco@gmail.com>
2020-02-27 01:05:40 -0300

    Readd color to the logger

--- src/common/logger.cpp
+++ src/common/logger.cpp
@@ -13,14 +13,33 @@ void Logger::write(const char* tag, LogType type, const char* format, ...)
 {
 
 	const char* prefix;
-
+    const char* colorstart;
+    const char* colorend = "\033[0m";
+	
 	switch (type) {
-	case LogType::Debug: prefix = "[debug] "; break;
-	case LogType::Info: prefix = ""; break;
-	case LogType::Warning: prefix = "[warn] "; break;
-	case LogType::Error: prefix = "[error] "; break;
-	case LogType::Fatal: prefix = "[fatal] "; break;
-	default: prefix = ""; break;
+	case LogType::Debug:
+        prefix = "[debug] ";
+        colorstart = "\033[3m";
+        break;
+	case LogType::Info:
+        prefix = "";
+        colorstart = "";
+        break;
+	case LogType::Warning:
+        prefix = "[warn] ";
+        colorstart = "\033[33m";
+        break;
+	case LogType::Error:
+        prefix = "[error] ";
+        colorstart = "\033[31m";
+        break;
+	case LogType::Fatal:
+        prefix = "[fatal] ";
+        colorstart = "\033[31;1m";
+        break;
+	default:
+        prefix = "";
+        break;
 	}
 
 	char msg[1024] = {};
@@ -35,9 +54,9 @@ void Logger::write(const char* tag, LogType type, const char* format, ...)
     mtx.lock();
 
     if (!tag || tag[0] == '\0')
-        fprintf(_out, "%s%s\n", prefix, msg);
+        fprintf(_out, "%s%s%s%s\n", colorstart, prefix, msg, colorend);
     else
-        fprintf(_out, "%s%s: %s\n", prefix, tag, msg);
+        fprintf(_out, "%s%s%s: %s%s\n", colorstart, prefix, tag, msg, colorend);
     
     mtx.unlock();
 }
