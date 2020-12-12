e3761695bec0288ce74cd5993df8234a351da476
Arthur Mendes <arthurmco@gmail.com>
2020-12-12 20:09:46 -0300

    tests: add a missing virtual method on test window

--- test/utils/test_window.hpp
+++ test/utils/test_window.hpp
@@ -8,15 +8,17 @@
 
 #include <client/graphical/window.hpp>
 #include <client/input/input_service.hpp>
+#include <string>
 #include <unordered_map>
 
-#include "test_renderer.hpp"
 #include "test_gui_manager.hpp"
+#include "test_renderer.hpp"
 
 class TestWindow : public familyline::graphics::Window
 {
 private:
     TestRenderer* tr = nullptr;
+
 public:
     virtual void getSize(int& w, int& h) const
     {
@@ -33,23 +35,35 @@ public:
      */
     virtual void getFramebufferSize(int& width, int& height) { this->getSize(width, height); }
 
-    virtual void show(){}
+    virtual void show() {}
     virtual void setFramebuffers(
-        familyline::graphics::Framebuffer* f3D, familyline::graphics::Framebuffer* fGUI){}
-    virtual void update()                                                                {}
+        familyline::graphics::Framebuffer* f3D, familyline::graphics::Framebuffer* fGUI)
+    {
+    }
+    virtual void update() {}
 
-    virtual ~TestWindow() {if (tr) delete tr;}
+    virtual ~TestWindow()
+    {
+        if (tr) delete tr;
+    }
 
-    virtual familyline::graphics::Renderer* createRenderer() {
+    virtual familyline::graphics::Renderer* createRenderer()
+    {
         tr = new TestRenderer{};
         return this->getRenderer();
     }
-    virtual familyline::graphics::Renderer* getRenderer() {
+    virtual familyline::graphics::Renderer* getRenderer()
+    {
         return (familyline::graphics::Renderer*)tr;
     }
 
-    virtual familyline::graphics::gui::GUIManager* createGUIManager() {
-        auto& ima = familyline::input::InputService::getInputManager();        
+    virtual familyline::graphics::gui::GUIManager* createGUIManager()
+    {
+        auto& ima = familyline::input::InputService::getInputManager();
         return new TestGUIManager{*this, 800, 600, *ima.get()};
-    }    
+    }
+
+    virtual void showMessageBox(
+        std::string title, familyline::graphics::SysMessageBoxFlags flags, std::string content)
+        {}
 };
