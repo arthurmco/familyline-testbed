50e88927bdd94984ca22a078c28b6ba9c5fb62ac
Arthur Mendes <arthurmco@gmail.com>
2020-12-12 22:17:16 -0300

    Clarify a little bit the comments on how the input file format works

--- src/include/common/logic/input_recorder.hpp
+++ src/include/common/logic/input_recorder.hpp
@@ -24,14 +24,20 @@ namespace familyline::logic {
     
     /**
      * The input recorder file is divided into three parts:
-     *   - the header, marked by the FREC keyword, contain the length, version,
-     *     player information and object checksum data
+     *   - the header, marked by the FREC keyword, 4 bytes contain the version, 
+     *     4 bytes with the header length, and flatbuffer data with player information 
+     *     and object checksum data.
      *   - the inputs; each input is marked by the FINP keyword. We mark the inputs
      *     because, in the case of an unexpected crash, we can use this file for
      *     debugging; the file will be incomplete, but we can find the input commands
      *     thanks to those keywords.
-     *   - the footer, marked by the FEND keyword, contains checksum and input data
-     *     count
+     *   - the footer, marked by the FEND keyword, contains 4 bytes of keyword, 4 bytes of 
+     *     input data count and 4 bytes of checksum
+     *
+     * We do not store the size of the whole file, because this is more like of a streaming
+     * format; it is made to be recoverable and readable even if it is a little bit corrupted, 
+     * because of a crash.
+     *
      */    
     class InputRecorder {
     private:
