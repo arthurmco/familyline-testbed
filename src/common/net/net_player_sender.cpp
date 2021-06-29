8cd499345863c9cee821328573b102ca0969e673
Arthur Mendes <arthurmco@gmail.com>
2021-06-28 23:12:07 -0300

    net+logic: Increase tick delta size
    This will probably solve some issues with packets coming too late.
    
    They still happen, but they are less frequent, and probably caused by
    some other thing.

--- src/common/net/net_player_sender.cpp
+++ src/common/net/net_player_sender.cpp
@@ -11,7 +11,14 @@ bool NetPlayerSender::listenPlayer(logic::PlayerInputAction a)
         return false;
     
     auto& log = LoggerService::getLogger();
-    log->write("net-player-sender", LogType::Debug, "sending packet to the server, tick=%zu", a.tick);
+    static const char* action_names[] = {
+        "CommandInput", "ObjectMove", "SelectAction", "AddSelectAction", "CreateSelectGroup",
+        "CreateEntity", "SelectGroup", "AddSelectGroup", "RemoveSelectGroup",
+        "CameraMove", "CameraRotate", "?", "?", "?"
+    };
+    
+    log->write("net-player-sender", LogType::Debug, "sending packet to the server, tick=%zu, type: %s", a.tick,
+               action_names[a.type.index()]);
     
     gps_.sendInputMessage(a);
     return true;
