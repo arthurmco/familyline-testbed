8a9aed9990feea3475980870186d4119758294cf
Arthur Mendes <arthurmco@gmail.com>
2021-10-28 15:43:15 -0300

    build: refactor the cmake build file to improve readability
    The CMakeLists.txt file was very disorganized, with a lot of repeated
    code and statements, and a lot of things in one single file, so I split
    them into different files.
    
    Also, I removed the requirement for a googletest submodule, because we
    now download the googletest data instead of cloning, to allow an easier
    setup.

---
+++ cmake/prelude.cmake
@@ -0,0 +1,30 @@
+# Forbid in-source builds.
+# More or less inspired by https://github.com/friendlyanon/cmake-init generated
+# prelude.
+
+if(CMAKE_SOURCE_DIR STREQUAL CMAKE_BINARY_DIR)
+  message(
+      FATAL_ERROR
+      "In-source builds are not supported."
+      "You may need to delete 'CMakeCache.txt' and 'CMakeFiles/' first."
+  )
+endif()
+
+
+# Export the compile_commands.json so that autocompleters will work.
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+
+# Detect if we are using vcpkg or not.
+# If we are, we will use the vcpkg names, and get all dependencies from there.
+# If not, we use pkg-config and cmake's find_package. Remember that you will 
+# probably need to pass the required directories by hand.
+if (DEFINED VCPKG_TARGET_TRIPLET)
+  set(FLINE_USE_VCPKG ON)
+else()
+  set(FLINE_USE_VCPKG OFF)
+endif()
+
+# Add our cmake directory, where we have custom Find*** scripts for cmake
+# to find some of our dependencies.
+set( CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake" )
--- src/common/generated.cmake
+++ src/common/generated.cmake
@@ -1,4 +1,4 @@
-# Rules that knows how to habdle flatbuffer files, and generates a header that
+# Rules that knows how to handle flatbuffer files, and generates a header that
 # knows how to read them
 #
 # Emits a dependency called input-flatbuffer, with a property
