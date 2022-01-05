e49cc6692cab7b90723b4833de7a9575f3bbd5a4
Arthur Mendes <arthurmco@gmail.com>
2022-01-04 21:08:14 -0300

    tools: make the asset fill script inform the model size
    Knowing the model size is good, so we can set the entity's size based
    on the model

--- tools/assetfill.py
+++ tools/assetfill.py
@@ -12,7 +12,7 @@ import logging
 import sys
 import os
 from dataclasses import dataclass
-from typing import Optional, List
+from typing import Optional, List, Tuple
 import yaml
 
 @dataclass
@@ -32,6 +32,9 @@ class ModelInfo:
     model_path: str
     material: List[ModelMaterial]
 
+    # The size is just for show, it is not really added to the file
+    size: Optional[Tuple[float, float, float]]
+
 
 def get_asset_name_from_path(path: str) -> str:
     _, tmp = os.path.split(path)
@@ -39,7 +42,7 @@ def get_asset_name_from_path(path: str) -> str:
 
     return name
 
-def guess_without_texture_info(model: str, texture: str, masset: str) -> ModelInfo:
+def guess_without_texture_info(model: str, texture: str, masset: str, msize = None) -> ModelInfo:
     if texture is None:
         raise RuntimeError("You should specify a texture on formats without file info")
 
@@ -50,21 +53,50 @@ def guess_without_texture_info(model: str, texture: str, masset: str) -> ModelIn
 
     return ModelInfo(masset, model, [
         ModelMaterial(None, None, [
-            ModelTexture(tasset, texture)])])
+            ModelTexture(tasset, texture)])], msize)
 
+
+def calculate_size(points: List[Tuple[float, float, float]]) -> Tuple[float, float, float]:
+    """
+    Calculate the width and length of the mesh, based on the list of every
+    point of the mesh
+    """
+    minx = 99999
+    miny = 99999
+    minz = 99999
+    maxx = -minx
+    maxy = -miny
+    maxz = -minz
+
+    for p in points:
+        px, py, pz = p
+        minx = min(minx, px)
+        miny = min(miny, py)
+        minz = min(minz, pz)
+        maxx = max(maxx, px)
+        maxy = max(maxy, py)
+        maxz = max(maxz, pz)
+
+    return (maxx-minx, maxy-miny, maxz-minz)
 
 def guess_obj_file(model: str, texture: str, masset: str) -> ModelInfo:
     with open(model, "r") as modelfile:
         mtllib = None
+        points = []
+        
         for l in modelfile.readlines():
             if l.startswith("mtllib"):
                 mtllib = l.replace("mtllib", "", 1).strip()
-                break
+            elif l.startswith("v "):
+                parts = l.split(" ")
+                points.append((float(parts[1]), float(parts[2]), float(parts[3])))
 
+        msize = calculate_size(points)
+            
         # TODO: extract texture from material
         if mtllib is None:
             logging.warn(f"OBJ: {model} has no material embedded!")
-            return guess_without_texture_info(model, texture, masset)
+            return guess_without_texture_info(model, texture, masset, msize)
 
         mtexes = []
         if texture is not None:
@@ -76,7 +108,7 @@ def guess_obj_file(model: str, texture: str, masset: str) -> ModelInfo:
 
         return ModelInfo(masset, model, [
             ModelMaterial(mtlassetname, 
-                os.path.join(modelpath, mtllib), mtexes)])
+                          os.path.join(modelpath, mtllib), mtexes)], msize)
 
 
 
@@ -136,13 +168,17 @@ def print_model_information(m: ModelInfo) -> str:
 
             matdicts.append(md)
 
-    print(repr(texdicts))
+    logging.debug(repr(texdicts))
 
     assetdict = [mdict]
     assetdict.extend(matdicts)
     assetdict.extend(texdicts)
 
     v = yaml.dump({"assets": assetdict})
+
+    if m.size is not None:
+        x, y, z = m.size
+        v += f"\n# size: {x:.3f} x {y:.3f} x {z:.3f}\n"
 
     # We dump a text that can be appended into the current YAML, so we replace the first line
     return v.replace("assets:", f"# asset data for model {m.model_name}\n", 1)
@@ -153,8 +189,11 @@ logging.basicConfig(level=logging.DEBUG)
 parser = argparse.ArgumentParser(description='Guesses asset dependencies and write a YAML description of them')
 parser.add_argument("model", type=str, help="The name of the model")
 parser.add_argument("--texture", 
-        help="an optional texture, for models that does not embed one", 
-        default=None)
+                    help="an optional texture, for models that does not embed one", 
+                    default=None)
+parser.add_argument("--no-material",
+                    action='store_true',
+                    help="do not use the embedded material")
 parser.add_argument("--asset", 
         help="The asset model name, Defaults to the filename if not specified", 
         default=None)
@@ -165,6 +204,12 @@ asset = args.asset if args.asset is not None else get_asset_name_from_path(args.
 
 try:
     minfo = guess_model_dependencies(args.model, args.texture, asset)
+    if args.no_material:
+        for m in minfo.material:
+            # doing this way, we do not mess with the texture
+            m.name = None
+            m.path = None
+            
     print(print_model_information(minfo))
 except RuntimeError as e:
     print(f"Error: {e.args[0]}", file=sys.stderr)
